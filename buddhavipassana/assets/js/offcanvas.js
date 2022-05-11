e82732189499271ff45b42e617830c28661652c4
UBUNTU <dynamicdreamz1>
2022-05-11 11:43:34 +0530

    Ensure page shifts down to reveal menu  #8

--- buddhavipassana/assets/js/offcanvas.js
+++ buddhavipassana/assets/js/offcanvas.js
@@ -1,7 +1,18 @@
 (function () {
   'use strict'
 
+  // menu js 
+  $('a[data-bs-toggle="dropdown"]').on('show.bs.dropdown', function () {
+    $(this).parents().find("nav").addClass("overflow_none");
+})
+
+$('a[data-bs-toggle="dropdown"]').on('hide.bs.dropdown', function () {
+    $(this).parents().find("nav").removeClass("overflow_none");
+})
+
+
   document.querySelector('#navbarSideCollapse').addEventListener('click', function () {
     document.querySelector('.offcanvas-collapse').classList.toggle('open')
   })
+
 })()
