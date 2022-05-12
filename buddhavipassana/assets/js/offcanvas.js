c7368a6c41a7ef38b021062744466512e4de6b0b
UBUNTU <dynamicdreamz1>
2022-05-12 12:11:24 +0530

    update menu

--- buddhavipassana/assets/js/offcanvas.js
+++ buddhavipassana/assets/js/offcanvas.js
@@ -4,12 +4,10 @@
   // menu js 
   $('a[data-bs-toggle="dropdown"]').on('show.bs.dropdown', function () {
     $(this).parents().find("nav").addClass("overflow_none");
-    $(this).parents().find(".nav-scroller").addClass('move-down');
 })
 
 $('a[data-bs-toggle="dropdown"]').on('hide.bs.dropdown', function () {
     $(this).parents().find("nav").removeClass("overflow_none");
-    $(this).parents().find(".nav-scroller").removeClass('move-down');
 })
 
 
